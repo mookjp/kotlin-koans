6f68cdba851241999f2ac5df9581b3bbee99a9f2
mook <mookjpy@gmail.com>
2018-07-26 21:49:26 +0900

    add practice

--- src/ii_collections/n23CompoundTasks.kt
+++ src/ii_collections/n23CompoundTasks.kt
@@ -9,11 +9,18 @@ fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> {
 fun Customer.getMostExpensiveDeliveredProduct(): Product? {
     // Return the most expensive product among all delivered products
     // (use the Order.isDelivered flag)
-    todoCollectionTask()
+    return orders.filter { it.isDelivered }
+            .flatMap { it.products }
+            .maxBy { it.price }
+//    todoCollectionTask()
 }
 
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
     // Return the number of times the given product was ordered.
     // Note: a customer may order the same product for several times.
-    todoCollectionTask()
+    return customers.flatMap { it.orders }
+            .flatMap { it.products }
+            .filter { it.name == product.name }
+            .count()
+//    todoCollectionTask()
 }
